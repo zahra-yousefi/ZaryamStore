c3a7999bd967a2a973a67480028e2cdac2065143
zahra-yousefi <z.yoosefi6@gmail.com>
2016-05-08 19:28:07 +0430

    first commit

---
+++ app/src/main/java/zaryam/library2/BookTypeActivity.java
@@ -0,0 +1,13 @@
+package zaryam.library2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_type);
+    }
+}
---
+++ app/src/main/java/zaryam/library2/MainActivity.java
@@ -0,0 +1,24 @@
+package zaryam.library2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,BookTypeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
